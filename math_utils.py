d8fad764d4fbfb18c50468ca138be7a9160723a4
Ved Patel <113617528+Vedpatel2708@users.noreply.github.com>
2026-06-09 14:38:31 +0530

    Implement divide and multiply functions
    Added functions to divide and multiply numbers.

---
+++ math_utils.py
@@ -0,0 +1,7 @@
+def divide_numbers(a, b):
+    # Warning: This is a bug! It will crash if b is 0.
+    return a / b
+
+def multiply_numbers(x, y):
+    """Multiplies two numbers together."""
+    return x * y
